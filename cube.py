55850a87e18cff917319e522cd4750cebe873d95
wHui <598347094@qq.com>
2020-05-12 20:49:59 +0800

    Firsr Commit

---
+++ cube.py
@@ -0,0 +1,103 @@
+import pygame
+from random import *
+
+class Cube(pygame.sprite.Sprite):
+	cube_shapes = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
+	cube_colors = [
+	(0xcc, 0x99, 0x99), (0xff, 0xff, 0x99), (0x66, 0x66, 0x99),
+	(0x99, 0x00, 0x66), (0xff, 0xcc, 0x00), (0xcc, 0x00, 0x33),
+	(0xff, 0x00, 0x33), (0x00, 0x66, 0x99), (0xff, 0xff, 0x33),
+	(0x99, 0x00, 0x33), (0xcc, 0xff, 0x66), (0xff, 0x99, 0x00)
+	]
+	# 不同方块对应的形态, (0,0)为中心块, 前面代表行位置(向左减一，向右加一),后面代表列位置(向上减一,向下加一), 列表长度代表形态个数, (,)个数代表方块个数
+	I = [[(0, -1), (0, 0), (0, 1), (0, 2)],
+		 [(-1, 0), (0, 0), (1, 0), (2, 0)]]
+	J = [[(-2, 0), (-1, 0), (0, 0), (0, -1)],
+		 [(-1, 0), (0, 0), (0, 1), (0, 2)],
+		 [(0, 1), (0, 0), (1, 0), (2, 0)],
+		 [(0, -2), (0, -1), (0, 0), (1, 0)]]
+	L = [[(-2, 0), (-1, 0), (0, 0), (0, 1)],
+		 [(1, 0), (0, 0), (0, 1), (0, 2)],
+		 [(0, -1), (0, 0), (1, 0), (2, 0)],
+		 [(0, -2), (0, -1), (0, 0), (-1, 0)]]
+	O = [[(0, 0), (0, 1), (1, 0), (1, 1)]]
+	S = [[(-1, 0), (0, 0), (0, 1), (1, 1)],
+		 [(1, -1), (1, 0), (0, 0), (0, 1)]]
+	T = [[(0, -1), (0, 0), (0, 1), (-1, 0)],
+		 [(-1, 0), (0, 0), (1, 0), (0, 1)],
+		 [(0, -1), (0, 0), (0, 1), (1, 0)],
+		 [(-1, 0), (0, 0), (1, 0), (0, -1)]]
+	Z = [[(0, -1), (0, 0), (1, 0), (1, 1)],
+		 [(-1, 0), (0, 0), (0, -1), (1, -1)]]
+	#cube形态索引
+	cube_form = {
+		'I': I, 'J': J, 'L': L, 'O': O, 'S': S, 'T': T, 'Z': Z
+	}
+
+	def __init__(self, G_w_num = (400 // 25)):
+		super().__init__()
+		# cube类型
+		self.shape = self.cube_shapes[randint(0, len(self.cube_shapes) - 1)]
+		self.form = randint(0, len(self.cube_form[self.shape]) - 1)
+		# cube颜色
+		self.color = self.cube_colors[randint(0, len(self.cube_colors) - 1)]
+		# cube中心位置, 
+		self.center = (G_w_num // 2 - 1, 2)
+		# self.matrix = matrix
+
+	# 计算cube在左边网格中的位置
+	def get_pos1(self, center = None):
+		cube_cur = self.cube_form[self.shape][self.form]
+		if center is None:
+			center = self.center
+
+		return [(cube[0] + center[0], cube[1] + center[1]) for cube in cube_cur]
+	# 计算cube在右边预览处的位置
+	def get_pos2(self, center = (19.5, 4)):
+		cube_cur = self.cube_form[self.shape][self.form]
+		
+		return [(cube[0] + center[0], cube[1] + center[1]) for cube in cube_cur]
+
+	# 边缘检测
+	def conflict(self, center, matrix):
+		for cube in self.get_pos1(center):
+			# 超出屏幕之外,说明不合法
+			if cube[0] < 0 or cube[1] < 0 or cube[0] >= 16 or cube[1] >= 24:
+				return True
+			# 不为None，说明之前已经有小方块存在了，也不合法
+			if self.matrix[cube[0]][cube[1]]:
+				 return True
+		return False	
+
+	def draw_in_left(self, screen):
+		for cube in self.get_pos1():
+			pygame.draw.rect(screen, self.color, (cube[0] * 25, cube[1] * 25, 25, 25))
+			pygame.draw.rect(screen, (230, 230, 230), (cube[0] * 25, cube[1] * 25, 25, 25), 2)
+
+	def draw_in_right(self, screen):
+		for cube in self.get_pos2():
+			pygame.draw.rect(screen, self.color, (cube[0] * 25, cube[1] * 25, 25, 25))
+			pygame.draw.rect(screen, (230, 230, 230), (cube[0] * 25, cube[1] * 25, 25, 25), 2)
+
+	def moveLeft(self, matrix):
+		center = (self.center[0] - 1, self.center[1])
+		if not self.conflict(center, matrix):
+			self.center = center
+
+	def moveRight(self, matrix):
+		center = (self.center[0] + 1, self.center[1])
+		if not self.conflict(center, matrix):
+			self.center = center
+
+	def moveDown(self, matrix):
+		center = (self.center[0], self.center[1] + 1)
+		if not self.conflict(center, matrix):
+			self.center = center
+
+	def rotate(self, matrix):
+		new_form = (self.form + 1) % len(self.cube_form[self.shape])
+		old_form = self.form
+		self.form = new_form
+		if self.conflict(self.center, matrix):
+			self.form = old_form
+
---
+++ main.py
@@ -0,0 +1,106 @@
+import pygame, sys, os
+from pygame.locals import *
+from random import randint
+
+import cube
+
+# 设置窗口打开位置
+os.environ['SDL_VIDEO_WINDOW_POS'] = "400, 100"
+
+# 初始化
+pygame.init()
+pygame.mixer.init()
+size = w, h = 600, 600
+screen = pygame.display.set_mode(size, 0, 32)
+pygame.display.set_caption("俄罗斯方块")
+font = pygame.font.SysFont("impact", 24)
+FPS = 30
+
+# 格子大小
+G_SIZE = 25
+G_w_num = (w - 200) // 25
+G_h_num = h // 25
+
+# 加载音乐
+pygame.mixer.music.load('sound/bg.mp3')
+pygame.mixer.music.set_volume(0.5)
+
+# 颜色
+WHITE1 = (255, 255, 255)
+WHITE2 = (75, 75, 75)
+BLACK = (0, 0, 0)
+
+#文字绘制
+def print_text(font, x, y, text, color = WHITE1):
+	ti = font.render(text, True, color)
+	screen.blit(ti, (x, y))
+
+# 绘制网格
+def draw_grid():
+	for i in range((w - 200) // G_SIZE):
+		pygame.draw.line(screen, WHITE2, (i * G_SIZE, 0), (i * G_SIZE, 600), 1)
+	for i in range(h // G_SIZE):
+		pygame.draw.line(screen, WHITE2, (0, i * G_SIZE), (400, i * G_SIZE), 1)
+	pygame.draw.line(screen, WHITE1, (400, 0), (400, 600), 2)
+
+def clear_full(matrix, score):
+	new_matrix = []
+	full_number = 0
+	for i in matrix:
+		if any(i) is None:
+			new_matrix.append(matrix)
+		else:
+			score += 100
+	new_matrix = [[None] * G_w_num for i in range(G_h_num - len(new_matrix))].extend(new_matrix)
+
+	return new_matrix, score
+
+def main():
+	# pygame.mixer.music.play(-1)
+	clock = pygame.time.Clock()
+	# 使用矩阵记录方格是否为空
+	matrix = [[None] * G_w_num for i in range(G_h_num)]
+
+	# 分数
+	score = 0
+
+	live_cube = cube.Cube()
+	next_cube = cube.Cube()
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == KEYDOWN:
+				if event.key in [K_LEFT, K_a]:
+					live_cube.moveLeft(matrix)
+				elif event.key in [K_RIGHT, K_d]:
+					live_cube.moveRight(matrix)
+				elif event.key in [K_RIGHT, K_d]:
+					live_cube.moveRight(matrix)
+				# elif event.key in [K_DOWN, K_s]:
+				# 	live_cube.moveDown()
+				elif event.key in [K_UP, K_w]:
+					live_cube.rotate(matrix)
+				elif event.key == K_SPACE:
+					live_cube.moveDown()
+				elif event.key == K_ESCAPE:
+					pygame.quit()
+					sys.exit()
+
+		keys = pygame.key.get_pressed()
+		if keys[K_DOWN] or keys[K_s]:
+			live_cube.moveDown(matrix)
+
+		matrix, score = clear_full(matrix, score)
+
+		screen.fill(BLACK)
+		draw_grid()
+		live_cube.draw_in_left(screen)
+		next_cube.draw_in_right(screen)
+		pygame.display.update()
+		clock.tick(FPS)
+
+if __name__ == "__main__":
+	main()
